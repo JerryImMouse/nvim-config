484729c0745623a115c870ab108282a5908424b1
JerryImMouse <toperkas@gmail.com>
2026-04-01 12:25:58 +0500

    feat: nixos support

--- after/plugin/nvim-lspconfig.lua
+++ after/plugin/nvim-lspconfig.lua
@@ -5,34 +5,48 @@ local capabilities = vim.tbl_deep_extend(
 )
 
 local on_attach = function(_, bufnr)
-	local map = function(mode, lhs, rhs, desc)
-		vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc})
-	end
-
-	map("n", "K", vim.lsp.buf.hover, "Hover docs")
-	map("n", "gd", vim.lsp.buf.definition, "Go to definition")
-	map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
-	map("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
-	map("n", "go", vim.lsp.buf.type_definition, "Go to type definition")
-	map("n", "gr", vim.lsp.buf.references, "Go to references")
-	map("n", "gs", vim.lsp.buf.signature_help, "Signature help")
-
-	map("n", "<F2>", vim.lsp.buf.rename, "Rename symbol")
-	map({"n", "x"}, "<F3>", function()
-		vim.lsp.buf.format({async = true})
-	end, "Format code")
-	map("n", "<F4>", vim.lsp.buf.code_action, "Code action")
-
-	map("n", "<leader>sd", vim.diagnostic.open_float, "Show diagnostics")
-
+  local map = function(mode, lhs, rhs, desc)
+    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+  end
+  map("n", "K",          vim.lsp.buf.hover,           "Hover docs")
+  map("n", "gd",         vim.lsp.buf.definition,      "Go to definition")
+  map("n", "gD",         vim.lsp.buf.declaration,     "Go to declaration")
+  map("n", "gi",         vim.lsp.buf.implementation,  "Go to implementation")
+  map("n", "go",         vim.lsp.buf.type_definition,  "Go to type definition")
+  map("n", "gr",         vim.lsp.buf.references,      "Go to references")
+  map("n", "gs",         vim.lsp.buf.signature_help,  "Signature help")
+  map("n", "<F2>",       vim.lsp.buf.rename,          "Rename symbol")
+  map({"n","x"}, "<F3>", function() vim.lsp.buf.format({ async = true }) end, "Format")
+  map("n", "<F4>",       vim.lsp.buf.code_action,     "Code action")
+  map("n", "<leader>sd", vim.diagnostic.open_float,   "Show diagnostics")
 end
 
 local lspconfig = require('lspconfig')
-require('mason-lspconfig').setup_handlers({
-	function(server)
-		lspconfig[server].setup({
-			capabilities = capabilities,
-			on_attach = on_attach
-		})
-	end,
+
+local servers = {
+  "lua_ls",
+  "rust_analyzer",
+  "pyright",
+  "ts_ls",
+  "nixd",
+}
+
+for _, server in ipairs(servers) do
+  lspconfig[server].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+  })
+end
+
+-- nixd extra config
+lspconfig.nixd.setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = {
+    nixd = {
+      nixpkgs = {
+        expr = 'import (builtins.getFlake "/etc/nixos").inputs.nixpkgs { }',
+      },
+    },
+  },
 })
